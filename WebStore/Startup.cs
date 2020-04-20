cdf4bb6c092c7e4b7e15b381411b9de36d1fb9d9
Konstantin Suvorov <konsu@inbox.ru>
2020-04-20 13:48:59 +0600

    Занятие 2: добавлена мастер страница

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -36,6 +36,8 @@ namespace WebStore
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             var helloMsg = _configuration["CustomHelloWorld"];
             var logLevel = _configuration["Logging:LogLevel:Microsoft"];
 
