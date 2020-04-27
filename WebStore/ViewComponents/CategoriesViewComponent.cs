cb556b828fee863549039ddf08d9fa3faea20989
Konstantin Suvorov <konsu@inbox.ru>
2020-04-27 13:22:03 +0600

    Занятие 4: ViewComponent для категорий

---
+++ WebStore/ViewComponents/CategoriesViewComponent.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.Models;
+
+namespace WebStore.ViewComponents
+{
+    [ViewComponent(Name = "Cats")]
+    public class CategoriesViewComponent : ViewComponent
+    {
+        private readonly IProductService _productServise;
+
+        public CategoriesViewComponent(IProductService productService)
+        {
+            _productServise = productService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = GetCategories();
+
+            return View(categories);
+        }
+
+        private List<CategoryViewModel> GetCategories()
+        {
+            var categories = _productServise.GetCategories();
+
+            var parentSections = categories.Where(p => !p.ParentId.HasValue).ToArray();
+            var parentCategories = new List<CategoryViewModel>();
+            // получим и заполним родительские категории
+            foreach (var parentCategory in parentSections)
+            {
+                parentCategories.Add(new CategoryViewModel()
+                {
+                    Id = parentCategory.Id,
+                    Name = parentCategory.Name,
+                    Order = parentCategory.Order,
+                    ParentCategory = null
+                });
+            }
+
+            // получим и заполним дочерние категории
+            foreach (var CategoryViewModel in parentCategories)
+            {
+                var childCategories = categories.Where(c => c.ParentId == CategoryViewModel.Id);
+                foreach (var childCategory in childCategories)
+                {
+                    CategoryViewModel.ChildCategories.Add(new CategoryViewModel()
+                    {
+                        Id = childCategory.Id,
+                        Name = childCategory.Name,
+                        Order = childCategory.Order,
+                        ParentCategory = CategoryViewModel
+                    });
+                }
+                CategoryViewModel.ChildCategories = CategoryViewModel.ChildCategories.OrderBy(c => c.Order).ToList();
+            }
+            parentCategories = parentCategories.OrderBy(c => c.Order).ToList();
+            return parentCategories;
+        }
+    }
+}
