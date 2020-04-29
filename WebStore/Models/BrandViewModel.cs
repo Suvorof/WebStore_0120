5a24f246a577dec206dc05d9025a21a3e95de477
Konstantin Suvorov <konsu@inbox.ru>
2020-04-29 11:39:46 +0600

    Занятие 4: ДЗ. Добавлен View Component для  брендов

---
+++ WebStore/Models/BrandViewModel.cs
@@ -0,0 +1,12 @@
+﻿using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Models
+{
+    public class BrandViewModel : INamedEntity, IOrderedEntity
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public int Order { get; set; }
+        public int ProductCount { get; set; }
+    }
+}
